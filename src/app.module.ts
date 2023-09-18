5bfe21bd5aa9af479d00c094e79bc47244394923
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-09-18 00:54:37 -0500

    basic db config updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentModule } from './modules/student/student.module';
+import { PreTestModule } from './modules/pre-test/pre-test.module';
+import { PostTestModule } from './modules/post-test/post-test.module';
+import { MailListModule } from './modules/mail-list/mail-list.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/girls-stem-db',),
+    StudentModule,
+    PreTestModule,
+    PostTestModule,
+    MailListModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
