a06930a8b3529c3c7fe4762d0dbc4b4f04888670
Sebastian Santiago Alfaro Mendoza <sebas43243@hotmail.com>
2023-09-28 18:48:42 -0500

    hotfix: added vocational-test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,17 @@ import { PreTestModule } from './modules/pre-test/pre-test.module';
 import { PostTestModule } from './modules/post-test/post-test.module';
 import { MailListModule } from './modules/mail-list/mail-list.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { VocationalTestModule } from './modules/vocational-test/vocational-test.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/girls-stem-db'),
+    AuthModule,
     StudentModule,
+    VocationalTestModule,
     PreTestModule,
     PostTestModule,
     MailListModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
